35c44d51a2b00ae194e6a0f5092ba7d67cc3c147
Arthur Taylor <arthur.taylor@gmail.com>
2017-05-05 21:54:16 +0000

    sdfsd

--- perloin.rb
+++ perloin.rb
@@ -1,48 +1,112 @@
 require 'serialport' # NOTE: you probably need to install this gem with: 'gem install serialport'
-require_relative 'arduino-lights'
-
 require 'perlin_noise'
 
+require_relative 'arduino-lights'
+require_relative 'utils'
 
 
-def noise_value_to_brightness(noise_value)
-  range = 128 # 253
-  (noise_value * range).to_i
-end
+def mixed_colors_pixel_arrays
+  n2d_red = Perlin::Noise.new 3
+  n2d_green = Perlin::Noise.new 3
+  n2d_blue = Perlin::Noise.new 3
 
-def xy_to_pixel_number(x,y)
-  row_offset = y * 12
-  if(y % 2 == 0)
-    column_offset = (11 - x)
-  else
-    column_offset = x
-  end
+  puts "started generating"
 
-  row_offset + column_offset
-  # (y * 12) + x
-end
+  (0..$z_layers).map do |z|
+    (0..11).map do |x|
+      (0..11).map do |y|
+        value = $contrast.call(n2d_red[x / 12.0, y/12.0, z/12.0])
+        red = noise_value_to_brightness(value)
 
+        value = $contrast.call(n2d_green[x / 12.0, y/12.0, z/12.0])
+        green = noise_value_to_brightness(value)
 
-n2d = Perlin::Noise.new 3
-contrast = Perlin::Curve.contrast(Perlin::Curve::CUBIC, 7)
+        value = $contrast.call(n2d_blue[x / 12.0, y/12.0, z/12.0])
+        blue = noise_value_to_brightness(value)
 
-
-(0..11).each do |z|
-  (0..11).each do |x|
-    #puts "next line"
-    (0..11).each do |y|
-      value = contrast.call(n2d[x / 12.0, y/12.0, z/12.0])
-      brightness = noise_value_to_brightness(value)
-
-
-      pixel_number = xy_to_pixel_number(x,y)
-      #puts "x #{x}, y: #{y}, pixel_number: #{pixel_number}"
-      #puts "value #{value}, brightness: #{brightness}"
-      ArduinoLights::set_pixel(pixel_number, brightness, 0, 0)
+        [red, green, blue]
+      end
     end
   end
-  
-  ArduinoLights::draw_screen
+  puts "finished generating"
 end
+
+def single_color_data
+  n2d = Perlin::Noise.new 3
+
+  puts "started generating"
+
+  data = (0..$z_layers).map do |z|
+    (0..11).map do |x|
+      (0..11).map do |y|
+        noise_value = $contrast.call(n2d[x / 12.0, y/12.0, z/12.0])
+
+        red = noise_value_to_brightness(noise_value)
+        green = 0 #noise_value_to_brightness(value)
+        blue = 0 #noise_value_to_brightness(value)
+
+        [red, green, blue]
+      end
+    end
+  end
+  puts "finished generating"
+  data
+end
+
+$contrast = Perlin::Curve.contrast(Perlin::Curve::CUBIC, 7)
+$z_layers = 100
+pixel_data = single_color_data()
+
+while(true) do
+  (0..$z_layers).each do |z|
+    (0..11).each do |x|
+      (0..11).each do |y|
+        pixel_number = xy_to_pixel_number(x,y)
+
+        red, green, blue = pixel_data[z][x][y]
+
+
+        #puts "x #{x}, y: #{y}, pixel_number: #{pixel_number}"
+        #puts "value #{value}, brightness: #{brightness}"
+        ArduinoLights::set_pixel(pixel_number, red, green, blue)
+      end
+    end
+    ArduinoLights::draw_screen
+  end
+end
+
+
+# (0..11).each do |z|
+#   (0..11).each do |x|
+#     #puts "next line"
+#     (0..11).each do |y|
+#       t = Time.now
+#       value = contrast.call(n2d_red[x / 12.0, y/12.0, z/12.0])
+#       red = noise_value_to_brightness(value)
+
+#       value = contrast.call(n2d_green[x / 12.0, y/12.0, z/12.0])
+#       green = noise_value_to_brightness(value)
+
+#       value = contrast.call(n2d_blue[x / 12.0, y/12.0, z/12.0])
+#       blue = noise_value_to_brightness(value)
+
+#       pixel_number = xy_to_pixel_number(x,y)
+
+#       puts "calulation took: #{t - Time.now}"
+
+
+
+
+
+
+
+#       #puts "x #{x}, y: #{y}, pixel_number: #{pixel_number}"
+#       #puts "value #{value}, brightness: #{brightness}"
+#       ArduinoLights::set_pixel(pixel_number, red, green, blue)
+#     end
+#   end
+  
+#   ArduinoLights::draw_screen
+# end
 
 
